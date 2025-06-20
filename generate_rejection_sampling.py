7d876687d750ae436d73110fa8c8db58531e0e2d
yjianhao user <yjianhao@submit-0.fair-aws-h200-1.hpcaas>
2025-06-20 15:52:20 +0000

    visual sample add

--- generate_rejection_sampling.py
+++ generate_rejection_sampling.py
@@ -4,6 +4,8 @@ from transformers import AutoVideoProcessor, AutoModel
 from diffusers.utils import export_to_video
 import torch
 from compute_vjepa_score import get_score  # Import the process_video function
+import os
+os.environ['TOKENIZERS_PARALLELISM'] = 'false'
 
 # Available models: Wan-AI/Wan2.1-I2V-14B-720P-Diffusers or Wan-AI/Wan2.1-I2V-14B-480P-Diffusers
 model_id = "Wan-AI/Wan2.1-T2V-1.3B-Diffusers"
@@ -33,8 +35,9 @@ worst_frames = None
 
 for i in range(10):  # Try 10 times
     frames = pipe(prompt=prompt, negative_prompt=negative_prompt, num_frames=num_frames).frames[0]
-    export_to_video(frames, f"./temp/{tag}/wan-t2v{i}.mp4", fps=16)  # Export to video
+    
     score = get_score(frames, model, processor)  # Calculate the score
+    export_to_video(frames, f"./temp/{tag}/wan-t2v{i}_{score}.mp4", fps=16)  # Export to video
     print(f"{i}, Score: {score}")
     
     if score < best_score:  # Update the best score and frames
@@ -46,7 +49,7 @@ for i in range(10):  # Try 10 times
         worst_frames = frames
 
 print(f"Best Score: {best_score}")
-export_to_video(best_frames, "./temp/best_wan-t2v.mp4", fps=16)  # Export the best frames to video
+export_to_video(best_frames, f"./temp/{tag}/bes_{score}.mp4", fps=16)  # Export the best frames to video
 
 print(f"Worst Score: {worst_score}")
-export_to_video(worst_frames, "./temp/worst_wan-t2v.mp4", fps=16)  # Export the worst frames to video
+export_to_video(worst_frames,f"./temp/{tag}/worst_{score}.mp4", fps=16)  # Export the worst frames to video
